5e04e29350e4755f0bdbb39d2f70386fdbf3586b
manji6 <ryosuke.sawada@gmail.com>
2021-06-12 22:54:24 +0900

    added some function

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Airtable from "airtable";
+import axios from "axios";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import { Layout } from "./components/Layout";
+import { Default } from "./pages/Default";
 
 // Airtable settings
-new Airtable({apiKey:'key65gqCdfYa4ZQjs'}).base('appzv1gqnbxS4OW8F');
+new Airtable({ apiKey: "key65gqCdfYa4ZQjs" }).base("appzv1gqnbxS4OW8F");
 
 // base endpoint to call with each request
-axios.defaults.baseURL = 'https://api.airtable.com/v0/appzv1gqnbxS4OW8F/food_data';
+axios.defaults.baseURL =
+  "https://api.airtable.com/v0/appzv1gqnbxS4OW8F/food_data";
 
 // content type to send with all POST requests
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 // authenticate to the base with the API key
-axios.defaults.headers['Authorization'] = 'Bearer key65gqCdfYa4ZQjs'
+axios.defaults.headers["Authorization"] = "Bearer key65gqCdfYa4ZQjs";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Layout />, rootElement);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Layout>
+      <Route extract="/" component={Default}></Route>
+    </Layout>
+  </BrowserRouter>,
+  rootElement
+);
---
+++ src/pages/Default.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class Default extends React.Component {
+  render() {
+    return <h1>Default</h1>;
+  }
+}
